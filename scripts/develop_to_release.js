ec51d942bd9f64e54fadeb78357a2b2b9e74ddaf
myalniz <myyalniz@gmail.com>
2021-02-08 16:50:24 +0300

    develop to release train

---
+++ scripts/develop_to_release.js
@@ -0,0 +1,33 @@
+const fetch = require("node-fetch")
+const fs = require("fs")
+
+const JIRA_USERNAME = process.env.JIRA_USERNAME
+const JIRA_API_TOKEN = process.env.JIRA_API_TOKEN
+
+const event = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, "utf8"))
+
+
+async function main() {
+  await processCommits()
+}
+
+main()
+
+async function headersWithAuth(headers) {
+  const auth =
+    "Basic " + global.Buffer.from(JIRA_USERNAME + ":" + JIRA_API_TOKEN).toString("base64")
+  return Object.assign(headers, { Authorization: auth })
+}
+
+async function processCommits() {
+  var branch = event.ref.replace("refs/heads/", "")
+  var issueIdRegex = new RegExp("^[[A-Z]+-[0-9]+].+$")
+ 
+  var commitsUrls=event.pull_request.commits.href
+
+  console.log("Commits URL: " + commitsUrls)
+  
+}
+
+
+
