fcacb2251a8cd4e5e8f861a917fffac66a42f708
mustafayalniz-dev <mustafa.yalniz@spin.pm>
2022-03-29 15:51:08 +0300

    beep3

--- scripts/ghg.js
+++ scripts/ghg.js
@@ -20,7 +20,7 @@ async function main() {
     const setEmail = `git config --global user.email "githubaction@goldenheartsgames.com"`
     const setIdentity = `git config --global user.name "GHG Github Action"`
     const addAll = `git add .metadata.json`
-    const commitAll = `git commit -m "Github Action commits conflict"`
+    const commitAll = `git commit -m "Github Action commits metadata json"`
     const pushCurrentBranch = `git push origin ${currentBranch}`
 
 
